5fd4b9c910ce49a752449f5e822cc762caf78097
takaakikinki1128 <takaakikinki1128@icloud.com>
2019-08-25 00:25:29 +0900

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root  'products#index'
-  resources :products
+  resources :products do
+    collection do
+      get 'search'
+    end
+  end
   resources :users, only: [:show,:index] do
     collection do
       get 'search'
