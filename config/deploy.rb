eb37582d8df57b7e4c58f571bd45ca5a6d7577c3
takaakikinki1128 <takaakikinki1128@icloud.com>
2019-09-14 22:52:41 +0900

    ok

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,10 @@
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.11.0"
+# config valid only for current version of Capistrano
+# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
+lock '3.11.0'
 
-set :application, "next-beauty"
+# Capistranoのログの表示に利用する
+set :application, 'next-beauty'
+
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:takaakikinki1128/next-beauty.git'
 
@@ -9,11 +12,11 @@ set :repo_url,  'git@github.com:takaakikinki1128/next-beauty.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1' 
+set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/nextbeauty.pem'] 
+                  keys: ['~/.ssh/beauty.pem']  
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
