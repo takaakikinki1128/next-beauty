7603e6a2ed4d740940b5b99896fc4ff0ccdeadd8
takaakikinki1128 <takaakikinki1128@icloud.com>
2019-09-25 18:40:02 +0900

    ok

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,15 +3,11 @@ set :application, 'next-beauty'
 set :repo_url,  'git@github.com:takaakikinki1128/next-beauty.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :linked_files, %w{ config/secrets.yml }
-
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' 
-
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/next-beauty.pem']  
-
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
-
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
